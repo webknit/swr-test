80a5d519e566ffedb8e1160c30ba9e3a516a3979
Shane Prendergast <contact@webknit.co.uk>
2022-03-17 08:55:20 +0000

    .

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+
+    setTimeout(() => {
+        res.status(200).json({ num: Math.floor(Math.random() * 1000) })
+    }, 5000)
+  
 }
